c91aa61fe5800841d01fb9db14a12d71a0595a62
Ira Baker <paxpistor@gmail.com>
2026-02-11 21:01:03 +0100

    Initial commit

---
+++ src/test/java/com/ilcarro/stepDefinitions/LoginSteps.java
@@ -0,0 +1,33 @@
+package com.ilcarro.stepDefinitions;
+
+import com.ilcarro.pages.HomePage;
+import com.ilcarro.pages.LoginPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+import static com.ilcarro.pages.BasePage.driver;
+
+public class LoginSteps {
+
+    @And("User clicks on Login link")
+    public void click_on_Login_link() {
+        new HomePage(driver).clickOnLoginLink();
+    }
+
+    @And("User enters correct data")
+    public void enters_correct_data() {
+        new LoginPage(driver).enterData("manuel@gm.com","Manuel1234!");
+    }
+
+    @And("User clicks on Yalla button")
+    public void clicks_on_Yalla() {
+        new LoginPage(driver).clickOnYalla();
+    }
+
+    @Then("User verifies Success message is displayed")
+    public void verifu_Success_message() {
+        new LoginPage(driver).verifyMessage("Logged in success");
+    }
+
+
+}
